3a7bc1792538c5f4c890f3cac5cec376340f817d
Myulee <bumblesam2.0@gmail.com>
2026-02-10 22:14:56 -0500

    Initial Basic commit

---
+++ AtYourCreate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using static Raylib_cs.Raylib;
+
+namespace AtYourCreate;
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int screenWidth = 1800;
+        const int screenHeight = 1200;
+        InitWindow(screenWidth, screenHeight, "P2ND");
+
+        while(!WindowShouldClose())
+        {
+            BeginDrawing();
+                ClearBackground(Raylib_cs.Color.Black);
+                DrawText("Hello world", 100, 100, 110, Raylib_cs.Color.White);
+            EndDrawing();        
+            
+        }
+    }
+}
